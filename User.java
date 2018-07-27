8b3bea2ee45cb7d2675308acf988c94a77bc71ca
Lucas Andrade Rodrigues <lucas.andrade.rodrigues.13@gmail.com>
2018-07-26 21:48:06 -0300

    Update User.java

--- User.java
+++ User.java
@@ -1,11 +1,11 @@
 public interface User {
-	public Post_simp simples();
+	public Post_simp criarSimp();
 
-	public Post_taf tarefa();
+	public Post_taf criarTarefa();
 
 	public void arquivar(Post_it postit);
 	
-	public void ver_post();
+	public void ver_arquivados();
 
 	public void buscar(String tag);
 	
